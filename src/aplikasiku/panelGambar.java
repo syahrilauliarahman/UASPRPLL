58498db32dd7e25982ad299181b90fc854c483cc
syahrilauliarahman <syahrilauliarahman@gmail.com>
2015-12-22 12:55:51 +0700

    versi 1

---
+++ src/aplikasiku/panelGambar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aplikasiku;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+/**
+ *
+ * @author Razaq Nice
+ */
+public class panelGambar extends JPanel{
+ 
+    private Image image; // membuat variable image
+     public panelGambar() {
+        image = new ImageIcon(getClass().getResource("/aplikasiku/gambar1.jpg")).getImage();
+        //memanggil sumber daya gambar
+    }
+     @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+     
+        Graphics gd = (Graphics2D) g.create();
+     
+        gd.drawImage(image, 0,0,getWidth(),getHeight(), this);
+        // menggambar image
+        gd.dispose();
+    }
+}
---
+++ test/aplikasiku/panelgambar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aplikasiku;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Syahril
+ */
+class panelgambar {
+
+    void paintComponent(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
